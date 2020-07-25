24ad5e38582b65a9ae87e88d054d282b086f0f27
Artxe2 <balgo@DESKTOP-EU0HILL>
2020-07-25 10:34:28 +0900

    asd

--- src/main/java/project/web/base/HashMapBinder.java
+++ src/main/java/project/web/base/HashMapBinder.java
@@ -1,6 +1,5 @@
 package project.web.base;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Enumeration;
@@ -16,7 +15,8 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class HashMapBinder {
 	private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(HashMapBinder.class);
-	private static final String rootPath= "C:\\workspace\\baseSpring\\src\\main\\webapp";
+	//	private static final String rootPath= "C:\\workspace\\baseSpring\\src\\main\\webapp";
+	private static final String UPLOADFOLDER = new java.io.File("src/main/webapp/resources/uploaded_files").getAbsolutePath();
 
 	public static Map<String, Object> getParameterMap(HttpServletRequest request) {
 		logger.info("getParameterMap() ");
@@ -32,47 +32,41 @@ public class HashMapBinder {
 
 	public static Map<String, Object> getMultipartMap(HttpServletRequest request, String path) throws ServletException, IOException{
 		Iterator<String> i = Arrays.asList(path.split("/")).iterator();
-		StringBuilder fullPath = new StringBuilder(rootPath);
+		StringBuilder fullPath = new StringBuilder(UPLOADFOLDER);
 		while (i.hasNext()) {
 			fullPath.append("\\");
 			fullPath.append(i.next());
 		}
 		Map<String, Object> pMap = new HashMap<>();
-		logger.info("request.getContentType() : " + request.getContentType());
-
-		logger.info("getMultipartMap() ");
-		MultipartRequest multi = new MultipartRequest(request, fullPath.toString(), 10 * 1024 * 1024, "UTF-8",
+		MultipartRequest multi = new MultipartRequest(request, fullPath.toString(), 50 * 1024 * 1024, "UTF-8",
 				new DefaultFileRenamePolicy());
 		Enumeration<String> names = multi.getParameterNames();
-		logger.info(rootPath + path);
+		String name;
 		while (names.hasMoreElements()) {
-			String name = names.nextElement();
-			logger.info(name + ": " + multi.getParameter(name));
+			name = names.nextElement();
 			pMap.put(name, multi.getParameter(name));
+			logger.info(name + ", " + multi.getParameter(name));
 		}
-
-		try {
-			Enumeration<String> files = multi.getFileNames();
-			File file;
-			if (files != null) {
-				while (files.hasMoreElements()) {
-					//					logger.info("files.hasMoreElements()");
-					String fname = files.nextElement();
-					logger.info("fname:" + fname);
-					String filePathName = path + "/" + multi.getFilesystemName(fname);
-					if (filePathName != null && filePathName.length() > 1) {
-						file = new File(filePathName);
-						pMap.put(fname, filePathName);
-						double size = file.length() / 1024;//kb
-						pMap.put("file_size", String.valueOf(size));
-						logger.info("file: " + file);
-					}
-				}
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		//		try {
+		//			Enumeration<String> files = multi.getFileNames();
+		//			File file;
+		//			if (files != null) {
+		//				while (files.hasMoreElements()) {
+		//					//					logger.info("files.hasMoreElements()");
+		//					String fname = files.nextElement();
+		//					logger.info("fname:" + fname);
+		//					String filePathName = path + "/" + multi.getFilesystemName(fname);
+		//					if (filePathName != null && filePathName.length() > 1) {
+		//						file = new File(filePathName);
+		//						pMap.put(fname, filePathName);
+		//						logger.info(fname + ", " + filePathName);
+		//					}
+		//				}
+		//			}
+		//
+		//		} catch (Exception e) {
+		//			e.printStackTrace();
+		//		}
 		return pMap;
 	}
 }
