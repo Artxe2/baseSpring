9b942d491f40b33c91b96ef1be47c9fa55591fbb
Artxe2 <balgo@DESKTOP-EU0HILL>
2020-07-26 01:36:46 +0900

    asd

--- src/main/java/project/web/socket/WsHandler.java
+++ src/main/java/project/web/socket/WsHandler.java
@@ -1,7 +1,9 @@
 package project.web.socket;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.springframework.web.socket.CloseStatus;
@@ -12,50 +14,106 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class WsHandler extends TextWebSocketHandler {
 	org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(WsHandler.class);
 	List<WebSocketSession> sessionList = new ArrayList<>();
+	Map<String, String> names = new LinkedHashMap<>();
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		logger.info("afterConnectionEstablished({})", session);
+		logger.info("afterConnectionEstablished({})", session.getId());
+	}
+
+	private void broadCast(List<WebSocketSession> list, TextMessage message) {
+		logger.info("broadCast({}, {})", list.size(), message);
+		try {
+			for (WebSocketSession ws : list) {
+				ws.sendMessage(message);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		logger.info("handleTextMessage({},{})", session.getId(), message.getPayload());
-		session.sendMessage(message);
-		String msg_chat = "100";// 일반대화(1:n대화)
-		String msg_null = "101";// 빈 문자가 넘어올때
-		String msg_exit = "500";// 방 나갈때
+		logger.info("handleTextMessage({}, {})", session.getId(), message.getPayload());
+		StringTokenizer st = new StringTokenizer(message.getPayload(), Pvo.Sharp);
+		if (st.hasMoreTokens()) {
+			String protocol = st.nextToken();
+			if (protocol.compareTo(Pvo.A) == 0) {
+				methodA(session, st);
+			} else if (protocol.compareTo(Pvo.B) == 0) {
+				methodB(session, st);
+			} else if (protocol.compareTo(Pvo.LOG_IN) == 0) {
+				logIn(session, st);
+			} else if (protocol.compareTo(Pvo.LOG_OUT) == 0) {
+				session.close();
+			} else {
+				System.out.println("Undefined Protocol...\"" + protocol + "\"");
+			}
+		} else {
+			System.out.println("Not Included  Protocol...\"" + message.getPayload() + "\"");
+		}
+	}
 
-		StringTokenizer st = new StringTokenizer(message.getPayload(), "#");
-		String kind = st.nextToken();
-		// 메시지가 null체크
-		if (msg_null.equals(kind)) {
-			String info = "빈 공간을 입력했습니다.";
-			for (WebSocketSession sess : sessionList) {
-				sess.sendMessage(new TextMessage(info));
+	private void logIn(WebSocketSession session, StringTokenizer st) {
+		String userName = st.nextToken();
+		names.put(session.getId(), userName);
+		try {
+			for (WebSocketSession ws : sessionList) {
+				StringBuilder msg = new StringBuilder();
+				msg.append(Pvo.LOG_IN);
+				msg.append(Pvo.Sharp);
+				msg.append(names.get(ws.getId()));
+				msg.append(Pvo.Sharp);
+				msg.append(" - Welcome");
+				session.sendMessage(new TextMessage(msg.toString()));
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		// 메시지가 null 이 아닌 경우
-		else {
-			// 100번 일반대화 일때
-			if (msg_chat.equals(kind)) {
-				String msg = st.nextToken();
-				for (WebSocketSession sess : sessionList) {
-					sess.sendMessage(new TextMessage(session + msg));
-				}
-			}
-			// 500번 나가기 일 때
-			if (msg_exit.equals(kind)) {
-				String msg = st.nextToken();
-				for (WebSocketSession sess : sessionList) {
-					sess.sendMessage(new TextMessage(session + msg));
-				}
-			}
+		StringBuilder msg = new StringBuilder();
+		msg.append(Pvo.LOG_IN);
+		msg.append(Pvo.Sharp);
+		msg.append(userName);
+		msg.append(Pvo.Sharp);
+		msg.append(" - Hi");
+		broadCast(sessionList, new TextMessage(msg.toString()));
+		sessionList.add(session);
+		logger.info("sessionList.size(): {}", sessionList.size());
+	}
+
+	private void methodA(WebSocketSession session, StringTokenizer st) {
+		StringBuilder msg = new StringBuilder();
+		msg.append(Pvo.MESSAGE);
+		while (st.hasMoreTokens()) {
+			msg.append(Pvo.Sharp);
+			msg.append(st.nextElement());
 		}
+		try {
+			session.sendMessage(new TextMessage(msg.toString()));
+		} catch(Exception e) {
+		}
+	}
+
+	private void methodB(WebSocketSession session, StringTokenizer st) {
+		StringBuilder msg = new StringBuilder();
+		msg.append(Pvo.MESSAGE);
+		while (st.hasMoreTokens()) {
+			msg.append(Pvo.Sharp);
+			msg.append(st.nextElement());
+		}
+		broadCast(sessionList, new TextMessage(msg.toString()));
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		logger.info("afterConnectionClosed({}, {})", session, status);
+		logger.info("afterConnectionClosed({}, {})", session.getId(), status);
+		sessionList.remove(session);
+		StringBuilder msg = new StringBuilder();
+		msg.append(Pvo.LOG_OUT);
+		msg.append(Pvo.Sharp);
+		msg.append(names.get(session.getId()));
+		msg.append(Pvo.Sharp);
+		msg.append(" - Bye");
+		broadCast(sessionList, new TextMessage(msg.toString()));
 	}
 }
--- src/main/webapp/web.socket/WsClient.jsp
+++ src/main/webapp/web.socket/WsClient.jsp
@@ -1,3 +1,4 @@
+<%@page import="project.web.socket.Pvo"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -6,54 +7,72 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 <script type="text/javascript">
-let userName;
-let socket;
-const msg_chat = "100\r\n"; //방채팅 
-const msg_null = "101\r\n"; //빈 메시지를 전송했을 때 
-const msg_exit = "500\r\n";
-	/* 채팅 기능 선언부  */
+	let userName;
+	let socket;
+	let A = 'A\r\n';
+	let B = 'B\r\n';
+	const LOG_IN = 'a';
+	const LOG_OUT = 'b';
+	const MESSAGE = 'Z';
+	
 	window.addEventListener('DOMContentLoaded', (e) => {
 		userName = prompt('Input your name');
-		document.querySelector("#user_name").innerHTML = document.querySelector("#user_name").innerHTML + '[ ' + userName + ' ]';
+		document.querySelector('#user_name').innerHTML = document.querySelector('#user_name').innerHTML + '[ ' + userName + ' ]';
+		A += userName + '\r\n';
+		B += userName + '\r\n';
 		connectWS();
-		document.querySelector("#msg").addEventListener('keydown', (evt) => {
+		document.querySelector('#msg').addEventListener('keydown', (evt) => {
 			if (evt.keyCode == 13) {
 				evt.preventDefault();
-				let msg = document.querySelector("#msg").value;
-				socket.send(msg_chat + msg);
+				if (document.querySelector('#s_method').value === 'A') {					
+					socket.send(A + document.querySelector('#msg').value);
+				} else {
+					socket.send(B + document.querySelector('#msg').value);
+				}
 			}
 		});
 	});
 	function connectWS() {
-		socket = new WebSocket("ws:\\\\localhost:8000\\base\\ws?");
-		socket.open = function(message) {
-			console.log(message);
-		};
-		// 서버로부터 메시지를 받았을 때
-		socket.onmessage = function(event) {
-			document.querySelector("#text_area").innerHTML += event.data + "<br>";
-			document.querySelector("#text_area").scrollTop = document.querySelector("#text_area").scrollHeight;
-		};
-
-		socket.onclose = function(event) {
-			console.log("Server disconnect");
+		socket = new WebSocket('ws:\\\\localhost:8000\\base\\ws?');
+		socket.onopen = (e) => {
+			console.log('Server connect');
+			socket.send(LOG_IN + '\r\n' + userName);
 		};
 		
-		socket.onerror = function(event) {
-			console.log("Server Error");
+		socket.onmessage = (e) => {
+			let a = e.data.split('\r\n');
+			if (a[0] === MESSAGE) {		
+				document.querySelector('#text_area').innerHTML += a[1] + ": " + a[2] + '<br>';
+				document.querySelector('#text_area').scrollTop = document.querySelector('#text_area').scrollHeight;
+			} else if (a[0] === LOG_IN) {
+				
+			} else if (a[0] === LOG_OUT) {
+				
+			}
 		};
+
+		socket.onclose = (e) => {
+			console.log('Server disconnect');
+		};
+		
+		socket.onerror = (e) => {
+			console.log('Server Error');
+		};
+	}
+	function refreshUserList() {
+		
 	}
 </script>
 </head>
 <body>
-	<div id="user_name">My Name: </div>
-	<div id="text_area"
-		style="border: 1px solid black; overflow: auto; width: 600px; height: 350px;"></div>
-	<select>
-		<option>A(to Me)</option>
-		<option>B(to All)</option>
+	<div id='user_name'>My Name: </div>
+	<div id='text_area'
+		style='border: 1px solid black; overflow: auto; width: 600px; height: 350px;'></div>
+	<select id="s_method">
+		<option value='A'>A(to Me)</option>
+		<option value='B'>B(to All)</option>
 	</select>
-	<input type="text" id="msg" value="" class="form-control">
-
+	<input type='text' id='msg' class='form-control'>
+	<div id='userList'></div>
 </body>
 </html>
